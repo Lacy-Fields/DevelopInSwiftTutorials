78f10c410a44f34ebe1307291d0ecd69ea5a0e78
Lacy Fields <ljfoxtrot1018@gmail.com>
2024-07-04 11:57:36 -0400

    Added Chapter 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DevelopInSwiftTutorials
  Learn to develop apps in Xcode using Swift and SwiftUI
+
+Each chapter is divided into folders with individual learning projects
---
+++ Views, structures, and properties/WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Lacy Fields on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
