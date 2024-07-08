7fb77672b0be76fc04c6dbc67fc137e620ff26bc
Lacy Fields <ljfoxtrot1018@gmail.com>
2024-07-08 16:41:26 -0400

    Chapter 3 added

---
+++ Layout and style/OnBoardingFlow/OnBoardingFlow/OnBoardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingFlowApp.swift
+//  OnBoardingFlow
+//
+//  Created by Lacy Fields on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
