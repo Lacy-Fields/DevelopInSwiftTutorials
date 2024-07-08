cfe9df153c0646af122e5bf1cef0fcca88ccb80f
Lacy Fields <ljfoxtrot1018@gmail.com>
2024-07-08 16:37:24 -0400

    Chapter 2 Completed
    Finished custom view for weather app.

--- Views, structures, and properties/WeatherForecast/WeatherForecast/ContentView.swift
+++ Views, structures, and properties/WeatherForecast/WeatherForecast/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForcast()
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
             
-            DayForcast()
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
     }
 }
@@ -21,14 +21,41 @@ struct ContentView: View {
     ContentView()
 }
 
-struct DayForcast: View {
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Mon")
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("High: 70")
-            Text("Low: 50")
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
         }
         .padding()
     }
