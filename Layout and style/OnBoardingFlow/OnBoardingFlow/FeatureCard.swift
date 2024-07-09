93a3a7bfb6fec74ba408d2bc6ccd7aafe39fec0c
Lacy Fields <ljfoxtrot1018@gmail.com>
2024-07-09 09:10:46 -0400

    Added Feature Card

---
+++ Layout and style/OnBoardingFlow/OnBoardingFlow/FeatureCard.swift
@@ -0,0 +1,21 @@
+//
+//  FeatureCard.swift
+//  OnBoardingFlow
+//
+//  Created by Lacy Fields on 7/9/24.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    let iconName: String
+    let description: String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
